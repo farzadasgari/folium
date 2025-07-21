f981d8e1769cf4c53e49a8a5d32de62fe2f29bb5
Farzad Asgari <khufarzadasgari@gmail.com>
2025-07-21 19:12:47 +0330

    finish desktop navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,19 @@
 const Navbar = () => {
     return (
         <nav className="fixed top-0 left-0 right-0 z-40 transition-all duration-500">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-red-600">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-red-400">
                 <div className="flex justify-between items-center py-4">
-                    <div>Farzad Asgari</div>
-                    <div>Menu1 Menu2 Menu3 Menu4</div>
+                    <div className="text-2xl font-bold text-purple-600">Farzad Asgari</div>
+                    <div className="hidden md:flex space-x-8">
+                        {
+                            ["home", "about", "skills", "projects", "contact"].map((item) => (
+                                <button className="capitalize hover:text-purple-400 transition-all duration-300 cursor-pointer font-medium relative group">
+                                    {item}
+                                    <span className="absolute -bottom-1 left-0 w-0 h-0.5 group-hover:bg-purple-400 group-hover:w-full transition-all duration-300"></span>
+                                </button>
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
         </nav>
