5bffa09fcd7bca40305d9c25507b62e87c0b9a9c
Farzad Asgari <khufarzadasgari@gmail.com>
2025-08-02 18:49:09 +0330

    add center div to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 const Footer = () => {
     return(
         <footer className="py-12 px-4 border-t border-purple-400/20 hover:border-purple-400/80 bg-white/50 backdrop-blur-sm transition-all duration-300">
+            <div className="max-w-6xl mx-auto">
             Footer
+            </div>
         </footer>
     )
 }
