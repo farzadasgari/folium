3129ac4d4aa935ecc78841154adc85237a66d5fa
Farzad Asgari <khufarzadasgari@gmail.com>
2025-08-02 18:45:12 +0330

    add animated hover to footer base

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
     return(
-        <footer className="py-12 px-4 border-t border-purple-400/20 bg-white/50 backdrop-blur-sm">
+        <footer className="py-12 px-4 border-t border-purple-400/20 hover:border-purple-400/80 bg-white/50 backdrop-blur-sm transition-all duration-300">
             Footer
         </footer>
     )
 }
 
-export default Footer
+export default Footer;
