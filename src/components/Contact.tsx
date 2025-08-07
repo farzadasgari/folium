732e5ba98880fcaac09d04fdf4a6b64a019ce2c8
Farzad Asgari <khufarzadasgari@gmail.com>
2025-08-07 22:00:21 +0330

    change success submit alert style

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -23,7 +23,6 @@ const Contact = () => {
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
         setIsSubmitted(true);
-        setTimeout(() => setIsSubmitted(false), 3000);
         setFormData({
             name: '',
             email: '',
@@ -156,9 +155,27 @@ const Contact = () => {
                         </form>
 
                         {isSubmitted && (
-                            <div className="mt-4 p-4 bg-green-400/30 border border-green-400 rounded-lg text-slate-800 text-center">
-                                Thank You! Your message has been sent
-                                successfully!
+                            <div
+                                id="toast-success"
+                                className="show-notification flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow-sm dark:text-gray-400 dark:bg-gray-800"
+                                role="alert"
+                            >
+                                <div className="inline-flex items-center justify-center shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+                                    <svg
+                                        className="w-5 h-5"
+                                        aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="currentColor"
+                                        viewBox="0 0 20 20"
+                                    >
+                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                    </svg>
+                                    <span className="sr-only">Check icon</span>
+                                </div>
+                                <div className="ms-3 text-sm font-normal">
+                                    Thank You! Your message has been sent
+                                    successfully!
+                                </div>
                             </div>
                         )}
                     </div>
