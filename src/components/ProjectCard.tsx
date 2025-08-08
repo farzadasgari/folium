e6584d4c2f85262a11e858241c81398b2af152b9
Farzad Asgari <khufarzadasgari@gmail.com>
2025-08-08 18:09:07 +0330

    automate topics badges

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,4 +1,5 @@
 const ProjectCard = () => {
+    const topics = ["python", "signal", "turbulance", "numerical", "package"];
     return (
         <div className="max-w-80 mx-auto">
             <div className="bg-white/10 backdrop-blur-sm rounded-2xl overflow-hidden border border-purple-400/20 hover:border-purple-400/80 transition-all duration-300 group flex flex-col h-full">
@@ -21,21 +22,13 @@ const ProjectCard = () => {
                         A Python package designed for efficient processing and analysis of acoustic Doppler velocimeter (ADV) data.
                     </p>
                     <div className="flex flex-wrap gap-2 mb-6">
-                        <span className="px-3 py-1 bg-purple-200 text-purple-900 border border-purple-200 text-xs rounded-full font-medium">
-                            python
-                        </span>
-                        <span className="px-3 py-1 bg-purple-200 text-purple-900 border border-purple-200 text-xs rounded-full font-medium">
-                            signal
-                        </span>
-                        <span className="px-3 py-1 bg-purple-200 text-purple-900 border border-purple-200 text-xs rounded-full font-medium">
-                            turbulance
-                        </span>
-                        <span className="px-3 py-1 bg-purple-200 text-purple-900 border border-purple-200 text-xs rounded-full font-medium">
-                            numerical
-                        </span>
-                        <span className="px-3 py-1 bg-purple-200 text-purple-900 border border-purple-200 text-xs rounded-full font-medium">
-                            package
-                        </span>
+                        {
+                            topics.map((topic) => (
+                                <span className="px-3 py-1 bg-purple-200 text-purple-900 border border-purple-200 text-xs rounded-full font-medium">
+                                    {topic}
+                                </span>
+                            ))
+                        }
                     </div>
 
                 </div>
