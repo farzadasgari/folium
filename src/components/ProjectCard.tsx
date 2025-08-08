21418bffbafd9e16c813700cfbee5f41df4c502a
Farzad Asgari <khufarzadasgari@gmail.com>
2025-08-08 18:36:54 +0330

    add card buttons

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,3 +1,5 @@
+import { Github } from "lucide-react";
+
 const ProjectCard = () => {
     const topics = ["python", "signal", "turbulance", "numerical", "package"];
     return (
@@ -24,13 +26,27 @@ const ProjectCard = () => {
                     <div className="flex flex-wrap gap-2 mb-6">
                         {
                             topics.map((topic) => (
-                                <span className="px-3 py-1 bg-purple-200 text-purple-900 border border-purple-200 text-xs rounded-full font-medium">
+                                <span className="px-3 py-1 bg-purple-200 hover:bg-purple-300 text-purple-900 border border-purple-200 text-xs rounded-full font-medium">
                                     {topic}
                                 </span>
                             ))
                         }
                     </div>
 
+                    <div className="flex gap-3 mt-auto">
+                        <a 
+                        href="#"
+                        target="_blank"
+                        className="flex-1 text-white bg-gradient-to-r from-purple-400 to-purple-600 hover:text-gray-100 hover:shadow-lg text-center py-2 px-4 rounded-lg text-sm font-medium transition-all duration-300 transform transform-gpu hover:scale-[1.01]"
+                        >
+                            Live Demo
+                        </a>
+                        <a href="#"
+                        className="text-slate-500 hover:text-purple-400 transition-all duration-300 transform transform-gpu hover:scale-[1.1] p-2 rounded-lg hover:bg-gray-500/5"
+                        >
+                            <Github className="w-5 h-5" />
+                            </a>
+                    </div>
                 </div>
 
             </div>
