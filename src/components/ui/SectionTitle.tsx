533b7362984d4161718fb386536d2df49e4b256d
Farzad Asgari <khufarzadasgari@gmail.com>
2025-08-09 23:16:56 +0330

    create title component

---
+++ src/components/ui/SectionTitle.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface Title {
+    text: string
+}
+
+const SectionTitle: React.FC<Title> = ({text}) => {
+    return (
+        <div className="text-center mb-16">
+            <h2 className="text-4xl md:text-5xl mb-6 pb-2 font-bold bg-gradient-to-r from-purple-400 to-purple-600 bg-clip-text text-transparent">
+                {text}
+            </h2>
+            <div className="w-24 h-1 bg-gradient-to-r from-purple-400 to-purple-600 rounded-full mx-auto"></div>
+        </div>
+    )
+}
+
+export default SectionTitle;
